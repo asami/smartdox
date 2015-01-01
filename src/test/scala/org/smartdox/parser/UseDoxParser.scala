012d48ae792c6e75b6e4666bc95306c5019856be
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2015-01-01 22:43:29 +0900

    Fixes parser.

--- src/test/scala/org/smartdox/parser/UseDoxParser.scala
+++ src/test/scala/org/smartdox/parser/UseDoxParser.scala
@@ -4,33 +4,42 @@ import scalaz._
 import Scalaz._
 import org.scalatest.Matchers
 import org.goldenport.scalatest.ScalazMatchers
+import org.smartdox._
 
-/**
+/*
  * @since   Jan. 27, 2012
- * @version Feb.  5, 2014
+ *  version Feb.  5, 2014
+ * @version Sep.  9, 2014
  * @author  ASAMI, Tomoharu
  */
 trait UseDoxParser extends Matchers with ScalazMatchers {
-  def parse_orgmode_simple(in: String, out: String) {
+  protected def parse_orgmode_simple(in: String, out: String) {
     parse_orgmode(in, 
         """<!DOCTYPE html><html><head/><body>%s</body></html>""".format(out))
   }
 
-  def parse_orgmode(in: String, out: String) {
+  protected def parse_document(in: String): Document = {
+    val result = DoxParser.parseOrgmode(in)
+    result should be ('successful)
+    result.get.asInstanceOf[Document]
+  }
+
+  protected def parse_orgmode(in: String, out: String): Dox = {
     val result = DoxParser.parseOrgmode(in)
     result should be ('successful)
     val dox = result.get
     dox.toString() should be (out)
+    dox
   }
 
-  def parse_orgmode_auto_title(in: String, out: String) {
+  protected def parse_orgmode_auto_title(in: String, out: String) {
     val result = DoxParser.parseOrgmodeAutoTitle(in)
     result should be ('successful)
     val dox = result.get
     dox.toString() should be (out)
   }
 
-  def parse_orgmode_z(in: String, out: String) {
+  protected def parse_orgmode_z(in: String, out: String) {
     val result = DoxParser.parseOrgmodeZ(in)
     result should success
     result match {
